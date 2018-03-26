6772fa8f07a0e00216d6070a738e6d3d91692ff8
Enes Keric <mail.enes.nu@gmail.com>
2018-03-26 21:07:50 +0200

    Last test.

---
+++ test.js
@@ -0,0 +1,28 @@
+{
+  "name": "wora",
+  "version": "1.0.4",
+  "main": "main.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/EnessenE/Wora.git"
+  },
+  "author": "Enes",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/EnessenE/Wora/issues"
+  },
+  "homepage": "https://github.com/EnessenE/Wora#readme",
+  "dependencies": {
+    "body-parser": "^1.18.2",
+    "discord.js": "^11.3.2",
+    "express": "^4.16.3",
+    "mysql": "^2.15.0",
+    "os": "^0.1.1",
+    "request": "^2.85.0"
+  },
+  "devDependencies": {},
+  "description": ""
+}
