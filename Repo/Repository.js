b206adb3ca504e48ca19642e7acafc6af1a40e95
Enes Keric <EnessenE@users.noreply.github.com>
2018-08-15 21:29:47 +0200

    Added Repository

---
+++ Repo/Repository.js
@@ -0,0 +1,17 @@
+﻿const config = require("./Settings/config.json");
+const db = require("./Context/" + config.SQL.Type + ".js");
+
+
+module.exports = {
+    GetData: function (what, where, target) {
+        return await db.GetData(what, where, target);
+    },
+
+    GetWebhooksFromServerID: function (serverid) {
+        return await db.GetWebhooksFromServerID(serverid);
+    },
+
+    GetServerFromWebhook: function (webhook) {
+        return await db.GetServerFromWebhook(webhook);
+    }
+}
