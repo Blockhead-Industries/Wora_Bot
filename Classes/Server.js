bee8b2b442aefedae9bbd52d4b021ec88b7fbbf4
Enes Keric <EnessenE@users.noreply.github.com>
2018-09-19 19:02:56 +0200

    Added class: Server

---
+++ Classes/Server.js
@@ -0,0 +1,35 @@
+﻿class Server {
+
+    constructor(id, name, owner, webhooks) {
+        this.id = id; // server id
+        this.name = name; //name 
+        this.owner = owner; //user object from discord.js
+        this.webhooks = webhooks; //array
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get webhooks() {
+        return this._webhooks;
+    }
+
+    set webhooks(value) {
+        this._webhooks = value;
+    }
+
+}
