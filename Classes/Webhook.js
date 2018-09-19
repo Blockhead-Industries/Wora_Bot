72e32e1b5d8077c353c86adaa03cd8c4e35c4725
Enes Keric <EnessenE@users.noreply.github.com>
2018-09-19 19:03:03 +0200

    Added class: Webhook

---
+++ Classes/Webhook.js
@@ -0,0 +1,36 @@
+﻿class Webhook {
+
+    constructor(id, source, target, server) {
+        this.id = id;
+        this.source = source;
+        this.target = target;
+        this.server = server;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+
+        this._name = value;
+    }
+
+    get source() {
+        return this._source;
+    }
+
+    set source(value) {
+        this._source = value;
+    }
+
+    get server() {
+        return this._id;
+    }
+
+    set server(value) {
+
+        this._server = value;
+    }
+
+}
