e83253e7ba0ed502022300bed39f33a0e1d2c90f
Enes Keric <EnessenE@users.noreply.github.com>
2018-09-21 22:24:30 +0200

    Created user class

---
+++ Classes/User.js
@@ -0,0 +1,51 @@
+﻿'use strict';
+
+module.exports = class User {
+
+    constructor(id, name, owner, webhooks) {
+        this._id = id; // server id
+
+
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+
+        this._name = value;
+    }
+
+    async getadminuser() {
+        return new Promise(async function (resolve, reject) {
+            var result = [];
+            config.settings.admins.forEach(async function (admin) {
+                var x = await getuser(admin);
+                result.push(x);
+            });
+            resolve(await result);
+        })
+    }
+
+    async sendmessage(message) {
+        var user = await getuser(id);
+        user.send(message);
+    }
+
+    async isadmin(id) {
+        return false;
+    }
+
+    async sendtoadmin(message) {
+        console.log(message);
+        if (admins == undefined) {
+            admins = await getadminuser();
+        }
+        console.log("Sending message: " + message.toString());
+        admins.forEach(function (admin) {
+            admin.send(message.toString());
+        });
+    }
+
+}
